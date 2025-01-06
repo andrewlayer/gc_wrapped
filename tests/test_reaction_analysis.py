0a021da310179dc56733ad79eeae26581aafa357
Mark Sigel <mark.sigel@gmail.com>
2025-01-05 20:21:56 -0500

    add test

--- tests/test_reaction_analysis.py
+++ tests/test_reaction_analysis.py
@@ -2,6 +2,7 @@ from analysis import reaction_analysis
 from unittest import TestCase
 from datetime import datetime
 from helpers.db import Message
+from unittest.mock import MagicMock
 
 messages = [Message(row_id=87, text='dummy message', type=1, date=datetime(2024, 12, 28, 4, 52, 33, 882479), is_emote=False, embedding=None, sender_name='Armaan'), Message(row_id=87, text='dummy message', type=1, date=datetime(2024, 12, 28, 4, 52, 33, 882479), is_emote=False, embedding=None, sender_name='Armaan'), Message(row_id=87, text='Loved an image', type=1, date=datetime(2024, 12, 28, 4, 52, 33, 882479), is_emote=False, embedding=None, sender_name='Armaan')]
 
@@ -11,3 +12,8 @@ class ClientTests(TestCase):
 
         self.assertEqual(result['Armaan']['frequency'], 1)
         self.assertEqual(result['Armaan']['reaction_frequency']['loved'], 1)
+
+    def test_react_percentage(self):
+        reaction_analysis.get_message_total = MagicMock(return_value={"Armaan": 4})
+        result = reaction_analysis.get_react_percentage(messages)
+        self.assertEqual(result['Armaan'], 25)
